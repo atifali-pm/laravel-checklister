05792cdc3a64ade7fdaaf5408528cf175ac2b2ae
Atif Ali <atif@monotote.com>
2021-08-27 17:18:02 +0500

    Sorting via Livewire done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,5 +10,5 @@ class Task extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public $fillable = ['checklist_id', 'name', 'description'];
+    public $fillable = ['checklist_id', 'name', 'description', 'position'];
 }
